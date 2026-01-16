126387508e22de2904ca0ef438c353148fd712b8
bnay14 <bnay14@gmail.com>
2026-01-16 20:42:32 +0545

    Infra updates

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "tf-state-ecs-demo-bucket"
+    key    = "prod/ecs/terraform.tfstate"
+    region = "us-east-1"
+
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
---
+++ outputs.tf
@@ -0,0 +1,31 @@
+output "cluster_name" {
+value = aws_ecs_cluster.main.name
+}
+
+output "service_security_group_id" {
+  value = aws_security_group.ecs_nodes.id 
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "execution_role_arn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "task_role_arn" {
+  value = aws_iam_role.ecs_task_role.arn
+}
+
+output "capacity_provider_name" {
+  value = aws_ecs_capacity_provider.main.name
+}
+
+output "ecr_nextjs" {
+value = aws_ecr_repository.ecr.repository_url
+}
+
+output "private_subnets_json" {
+  value = jsonencode(aws_subnet.private[*].id)
+}
